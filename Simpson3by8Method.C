55137432991d8fc23c26d4b1cc87d8bfc81637a3
Sujoy Manna <62413879+Sujoy-2811@users.noreply.github.com>
2021-01-12 15:18:43 +0530

    updated

---
+++ Simpson3by8Method.C
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+double function(double y, float x, int coeff[], int deg)
+{
+    for (int i = deg; i >= 0; i--)
+    {
+        y += coeff[i] * pow(x, i);
+    }
+    return y;
+}
+
+int main()
+{
+    double y[20] = {0};
+    float x[20], xn, x0, h, odd = 0, even = 0;
+    int i, intervals, coeff[20] = {0}, deg;
+
+    //Modication
+    printf("\n\n  Enter the Highest Order of Equation: ");
+    scanf("%d", &deg);
+
+    //take eqn
+    for (i = deg; i >= 0; i--)
+    {
+        printf("  Enter Coeffecient of X^%d: ", i);
+        scanf("%d", &coeff[i]);
+    }
+
+    //print eqn
+    printf("Given eqn is : ");
+    for (int i = deg; i > 0; i--)
+    {
+        if (coeff[i] != 0)
+        {
+            printf("%dX^%d ", coeff[i], i);
+            if (coeff[i - 1] >= 0 && i > 1)
+                printf("+ ");
+            else if (i == 1)
+            {
+                if (coeff[0] >= 0)
+                    printf("+ ");
+            }
+        }
+    }
+
+    printf("%d\n\n", coeff[0]);
+
+    //taking n, a,b
+    printf("Enter the number of intervals:");
+    scanf("%d", &intervals);
+    printf("Enter the value of x0:");
+    scanf("%f", &x0);
+    printf("Enter the value of xn:");
+    scanf("%f", &xn);
+    h = ((xn - x0) / intervals);
+    printf("h=%f\n", h);
+    x[0] = x0;
+    for (i = 1; i <= intervals; i++)
+    {
+        x[i] = x[i - 1] + h;
+    }
+    //calculating values of fx
+    for (int i = 0; i <= intervals; i++)
+    {
+        //(double y, float x, float coeff[], int deg)
+        y[i] = function(y[i], x[i], coeff, deg);
+    }
+
+    //printing table
+    printf("Table is:\n\n");
+    printf("    X           Y\n");
+    for (i = 0; i <= intervals; i++)
+    {
+        printf("%f     %f\n", x[i], y[i]);
+    }
+    h = 3 * h / 8;
+    for (i = 1; i < intervals; i++)
+    {
+        if (i % 2 != 0)
+        {
+            even += y[i] * 3;
+        }
+        else
+        {
+            odd += y[i] * 2;
+        }
+    }
+    printf("\n\nAnswer is: %f", (h * (y[0] + y[intervals] + odd + even)));
+    getch();
+    return 0;
+}
